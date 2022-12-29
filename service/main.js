2a54c392aa930fdf3720e998baa22eeed8347476
ssudo <ramasedang@gmail.com>
2022-12-29 17:09:03 +0700

    hadeh

--- service/main.js
+++ service/main.js
@@ -22,7 +22,6 @@ wa.on("message", async (topic, message) => {
   let msg = data.msg;
   let targetSender = data.targetSender;
   console.log(data);
-  sendSeen(targetSender, topic);
   switch (msg) {
     case "!halo":
       await sendWa(targetSender, "Halo juga " + data.name, topic);
