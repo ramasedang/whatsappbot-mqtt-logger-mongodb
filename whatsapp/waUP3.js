9c1e358e6a9cc0a3eb8ea24a099cd5f93e6d82e3
ssudo <ramasedang@gmail.com>
2022-12-31 11:18:01 +0700

    UPDATE

--- whatsapp/waUP3.js
+++ whatsapp/waUP3.js
@@ -1,61 +1,89 @@
 import pkg from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 import mqtt from "mqtt";
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
 
-const waMqtt = mqtt.connect("mqtt://8.219.195.118:1883");
 
-const { Client, LocalAuth } = pkg;
+
+const { Client, LocalAuth, RemoteAuth } = pkg;
 
 const waUP3 = () => {
-  const wa = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-      args: ['--no-sandbox']
-    },
-  });
+  mongoose
+    .connect(
+      "mongodb+srv://root:root@cluster0.ateetri.mongodb.net/%3FretryWrites=true&w=majority"
+    )
+    .then( () => {
+      const waMqtt = mqtt.connect("mqtt://8.219.195.118:1883");
+      const store = new MongoStore({ mongoose: mongoose });
 
-  wa.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true });
-  });
+      const wa = new Client({
+        authStrategy: new RemoteAuth({
+          store: store,
+          backupSyncIntervalMs: 300000,
+        }),
+        puppeteer: {
+          args: ["--no-sandbox"],
+        },
+      });
 
-  wa.on("ready", () => {
-    console.log("Client is ready!");
-  });
+      wa.on("qr", (qr) => {
+        qrcode.generate(qr, { small: true });
+      });
 
-  wa.on("message", async (message) => {
-    const isGroupMsg = message.from.includes("@g.us");
-    let typeChat = isGroupMsg ? "group" : "private";
-    let test = await message.getQuotedMessage();
-    let quote = test ? test.body : "null";
-    let name = (await message.getContact()).pushname;
-    let no_sender = (await message.getContact()).number;
-    let no_group = "null";
-    let targetSender = message.from;
-    if (typeChat == "group") {
-      no_group = message.from.split("@")[0];
-    }
-    let msg = message.body;
-    console.log(
-      JSON.stringify({ name, no_sender, no_group, msg, quote, targetSender })
-    );
-    waMqtt.publish(
-      "broker1",
-      JSON.stringify({ name, no_sender, no_group, msg, quote, targetSender })
-    );
-  });
+      wa.on("ready", () => {
+        console.log("Client is ready!");
+      });
 
-  waMqtt.on("connect", () => {
-    waMqtt.subscribe("broker2");
-  });
+      wa.on("message", async (message) => {
+        const isGroupMsg = message.from.includes("@g.us");
+        let typeChat = isGroupMsg ? "group" : "private";
+        let test = await message.getQuotedMessage();
+        let quote = test ? test.body : "null";
+        let name = (await message.getContact()).pushname;
+        let no_sender = (await message.getContact()).number;
+        let no_group = "null";
+        let targetSender = message.from;
+        if (typeChat == "group") {
+          no_group = message.from.split("@")[0];
+        }
+        let msg = message.body;
+        console.log(
+          JSON.stringify({
+            name,
+            no_sender,
+            no_group,
+            msg,
+            quote,
+            targetSender,
+          })
+        );
+        waMqtt.publish(
+          "broker1",
+          JSON.stringify({
+            name,
+            no_sender,
+            no_group,
+            msg,
+            quote,
+            targetSender,
+          })
+        );
+      });
 
-  waMqtt.on("message", async (topic, message) => {
-    var data = JSON.parse(message.toString());
-    let phone = data.phone_number;
-    let msg = data.message;
-    wa.sendMessage(phone, msg);
-  });
+      waMqtt.on("connect", () => {
+        waMqtt.subscribe("broker2");
+      });
 
-  wa.initialize();
+      waMqtt.on("message", async (topic, message) => {
+        var data = JSON.parse(message.toString());
+        let phone = data.phone_number;
+        let msg = data.message;
+        wa.sendMessage(phone, msg);
+      });
+
+      wa.initialize();
+    });
 };
 
 waUP3();
