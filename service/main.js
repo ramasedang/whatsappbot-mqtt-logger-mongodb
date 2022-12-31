dd99d9df367f517f95cb74cd48a4612e121857ce
ssudo <ramasedang@gmail.com>
2023-01-01 01:24:03 +0700

    .

--- service/main.js
+++ service/main.js
@@ -62,7 +62,7 @@ wa.on("message", async (topic, message) => {
     await deleteAll();
     await sendWa(targetSender, "Log berhasil dihapus", topic1);
   } else if ((msg[0] || msg) === "!presensi") {
-    await sendWa(targetSender, "Sedang mengambil data...", topic1);
+    await sendWa(targetSender, "Sedang melakukan presensi ...", topic1);
     let result = await presensi(msg[1]);
     await sendWa(targetSender, result, topic1);
   }
